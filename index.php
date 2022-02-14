cfb7a4b92294f28a22baa9c0cfc1fef3b5ac674b
hugo Bayet <hugodragon34@yahoo.fr>
2022-02-14 13:47:17 +0100

    rajout du css

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+   <html>
+    <head>
+        <link rel="stylesheet" href="styles/jolie.css" type="text/css" media="screen" />
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" /> 
+        <title>Bienvenue sur insta gaming</title>
+        <?php
+        include("bd.php") ;
+        ?>
+    </head>
+    <p id="bonjour">Bonjour et bienvenue sur insta gaming</p>
+</body>
+    <?php
+    $bdd = getBD();
+    ?>
+    <table >
+            <tr>
+                <th>Numero ID</th>
+                <th>Nom </th>
+                <th>Quantité en stock </th>
+                <th>Prix (en euros)</th>
+            </tr>
+
+            <tr>
+                    
+
+            </tr
+    </table>
+<body>
+
+    <footer>
+    <p><a href="contact/contact.html" target="_self">
+        Contact
+        </a></p>
+    </footer>
+
+    
