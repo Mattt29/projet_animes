d19d56b1b2182d792d0ba03fc67ac1780b8322ce
hugo Bayet <hugodragon34@yahoo.fr>
2022-03-03 19:12:50 +0100

    mise a jour de index et jolie.css

--- code/index.php
+++ code/index.php
@@ -4,7 +4,10 @@
         <link rel="stylesheet" href="jolie.css" type="text/css" media="screen" />
         <meta http-equiv="content-type" content="text/html; charset=utf-8" /> 
         <title>Bienvenue sur list'animes</title>
-        
+        <?php
+        include("bd.php") ;
+        $bdd = getBD();
+        ?>
     </head>
 
 
@@ -47,11 +50,39 @@
 
 
 <div id= "barre_recherche">
-                <form action="" class="formulaire">
-                <input class="champ" type="text" value="rechercher un anime, un profil..." />
-                
-                     
-                </form>
+<?php
+
+
+
+ 
+ 
+  
+ $rep = $bdd->query('select * from anime');
+ 
+ if(isset($_GET['titre_anime']) AND !empty($_GET['titre_anime'])) {
+    $q = htmlspecialchars($_GET['titre_anime']);
+    $articles = $bdd->query('SELECT titre_anime FROM anime WHERE titre_anime LIKE "%'.$q.'%" ORDER BY id_anime DESC');
+    if($articles->rowCount() == 0) {
+       $articles = $bdd->query('SELECT titre_anime FROM anime WHERE CONCAT(titre_anime, contexte_anime) LIKE "%'.$q.'%" ORDER BY id_anime DESC');
+    }
+ }
+  
+
+
+    
+    $rep -> closeCursor();           
+    
+    ?>
+
+
+
+<form method="GET">
+   <input type="search" name="titre_anime" placeholder="Recherche anime" />
+   <input type="submit" value="Valider" />
+</form>
+
+
+
 </div>
 </div>
 
@@ -62,24 +93,25 @@
     
     
         <tr>
-            <th>top 3 animes</th>
+            <th> Top 3 animes</th>
             <th> Notes </th>
+            <th> Titre </th>
         </tr>
         <tr>
         
         
         <?  
-        include("bd.php") ;
-        $bdd = getBD();
+        
         
         $rep = $bdd->query('select * from anime');
 
 
-        //echo "<img src= url(".$ligne['note_generale_anime'].")";
+        
         $i=0;
-        while( $i<4 && $ligne = $rep -> fetch() ){ 
+        while( $i<3 && $ligne = $rep -> fetch() ){ 
         echo "<tr> <td> <img src='".$ligne['image_url_anime']."'</td>
-        <td>".$ligne['note_generale_anime']."</td>";
+        <td>".$ligne['note_generale_anime']."</td><td>"
+        ."<a href ="."fiche_anime.php?id_art=".$ligne['id_anime'].">".$ligne['titre_anime']."</a>"."</td>";
         $i=$i+1;
         }   
         
@@ -102,18 +134,19 @@
         </tr>
         <tr>
         <?php
-        include("bd.php") ;
-        $bdd = getBD();
+        
         
         $rep = $bdd->query('select titre_discussion from discussion');
 
 
         
-        echo $i;
+        
         while( $i<4 && $ligne = $rep -> fetch() ){ 
         echo "<tr><td>".$ligne['titre_discussion']."</td>";
         $i=$i+1;
         } 
+
+        $rep -> closeCursor();
         ?> 
         </tr>
     </tbody>
