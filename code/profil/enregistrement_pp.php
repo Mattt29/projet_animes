0357a0c9fc050fe90af20855e3b35c769ff6d6b8
Matthieu Grammatico <matthieugrammatico@gmail.com>
2022-03-21 18:11:18 +0100

    ajustement du style des pages du profil

--- code/profil/enregistrement_pp.php
+++ code/profil/enregistrement_pp.php
@@ -18,6 +18,40 @@ session_start();
 	$id=$_SESSION['utilisateur']['id_utilisateur'];
 	$rep = $bdd->query('UPDATE utilisateur SET id_photo_de_profil='.$id_pp.' WHERE id_utilisateur='.$id);
 
+ 	echo '<meta http-equiv="Refresh" content="0; url=profil.php"/>';
+
+?>
+
+<title>Enregistrement</title>
+</head>
+
+
+<body>
+
+
+</body>
+
+</html>
+<?php
+session_start();
+?>
+<html>
+<head>
+
+  <link rel="stylesheet" href="../styles/main.css" type="text/css" media="screen" />
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" /> 
+        <script src="https://kit.fontawesome.com/c6c76fd424.js" crossorigin="anonymous"></script>
+        <img src=../img/background.png id=fondecran class=fondecran alt=/>
+
+  
+ <?php 
+ 
+	include('../bd.php');
+	$bdd =getBD();
+	$id_pp=$_GET['id_photo_de_profil'];
+	$id=$_SESSION['utilisateur']['id_utilisateur'];
+	$rep = $bdd->query('UPDATE utilisateur SET id_photo_de_profil='.$id_pp.' WHERE id_utilisateur='.$id);
+
  	echo '<meta http-equiv="Refresh" content="5; url=profil.php"/>';
 
 ?>
--- code/profil/profil.php
+++ code/profil/profil.php
@@ -19,6 +19,92 @@ session_start();
             	font-size: 100%;
             }
             #photo{
+            	position: fixed;
+            	left: 70%;
+            	top: 25%;
+					text-align: center;
+            }
+        </style>
+    </head>
+
+
+
+    <body>
+      <div class="acceuilhaut">
+		<div id="profil">
+		<div class="homepage"> 
+			<a href="../index.php"> <i class="fa-solid fa-house"></i> </a>
+		</div>
+
+		<div id = "connexion" >
+			<?php
+				if(!isset($_SESSION['utilisateur'])){ ?>
+			   <p id="Se_connecter"> <a href="../connexion/connexion.php"> Se connecter </a> </p>
+    			<?php }
+    			else {
+    				echo  '<br />';
+					echo "Bonjour ".$_SESSION['utilisateur']['pseudo'];
+				?>	
+
+	<p id="Se_deconnecter"> <a href="../connexion/deconnexion.php"> Se déconnecter </a> </p>
+
+	
+
+    <?php }?>
+</div>
+
+</div>
+
+<form method="GET" action="../recherche.php" autocomplete="on">
+    
+    <input type="search" name="recherche"  placeholder="Recherche anime.." id="barre_recherche"  />
+    <input type="submit" value="Valider" id="bouton_validé" />
+</form>
+
+</div>
+<div id="page_profil">
+        <?php if(isset($_SESSION['utilisateur'])) {
+		
+		$id=$_SESSION['utilisateur']['id_utilisateur'];
+	 	$profil = $bdd->query('SELECT * from utilisateur where id_utilisateur='.$id); 		
+			$ligne =$profil->fetch();
+				echo 'Nom : '.$ligne['nom'].'<br>';				
+				echo 'Prénom : '.$ligne['prenom'].'<br>';	
+				echo 'Pseudo : '.$ligne['pseudo'].'<br>';	
+				echo 'Adresse mail : '.$ligne['mail'].'<br>';	
+				
+				$profil->closeCursor();
+				?>
+        <p> <a href="../amis/liste_amis.php"> Liste d'amis </a> </p>
+        <p> <a href="../amis/demandes_amis.php"> Demandes d'amis </a> </p>
+        <p id="photo"> <a href="pp.php"> Changer de pp </a></p>
+<?php } ?>
+
+</div>
+
+    </body>
+
+</html><?php 
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="../styles/main.css" type="text/css" media="screen" />
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" /> 
+        <script src="https://kit.fontawesome.com/c6c76fd424.js" crossorigin="anonymous"></script>
+        <img src=../img/background.png id=fondecran class=fondecran alt=""/>
+        <title>Profil</title>
+        <?php
+        include("../bd.php") ;
+        $bdd = getBD();
+        ?>
+        <style>
+            #profil{
+            	font-size: 100%;
+            }
+            #photo{
             	position: absolute;
             	left: 70%;
             	top: 25%;
