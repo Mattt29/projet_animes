8b0eae122daa96d87ff017c51fa49e5acc9a3529
Matthieu Grammatico <matthieugrammatico@gmail.com>
2022-04-05 01:35:16 +0200

    reco presque parfaite

--- code/reco.php
+++ code/reco.php
@@ -20,49 +20,6 @@ session_start();
 
 
 <body>
-<!--
-	<div class=acceuilhaut>
-		<div id=profila>
-		<div class="homepage"> 
-			<a href="../index.php"> <i class="fa-solid fa-house"></i> </a>
-		</div>
-
-		<div id = "connexion" >
-			<?php
-				if(!isset($_SESSION['utilisateur'])){ ?>
-			   <p id="Se_connecter"> <a href="../connexion/connexion.php"> Se connecter </a> </p>
-    			<?php }
-    			else {
-    				echo  '<br />';
-					echo "Bonjour ";
-					echo $_SESSION['utilisateur']['pseudo'];
-				?>	
-
-	<p id="Se_deconnecter"> <a href="../connexion/deconnexion.php"> Se déconnecter </a> </p>
-
-	
-
-    <?php }?>
-
-
-
-</div>
-
-</div>
-
-
-
-
-
-<form method="GET" action="../recherche.php" autocomplete="on">
-    
-    <input type="search" name="recherche"  placeholder="Recherche anime.." id="barre_recherche"  />
-    <input type="submit" value="Valider" id="bouton_validé" />
-</form>
-
-</div>
-
--->
 
 <?php
 include("bd.php");
@@ -73,31 +30,36 @@ $id_utilisateur=$_SESSION['utilisateur']['id_utilisateur'];
 $nb=$bdd->query('SELECT id_utilisateur FROM utilisateur ORDER BY id_utilisateur DESC LIMIT 1');
 $id=0;
 while($ligne =$nb->fetch()) {	
-$id+=$ligne[0];
-}
-
-echo $id;
+	$id+=$ligne[0];
+}  
+  
 $j=1;
 while($j<=$id) {
-	echo $j;
-	$liste=$bdd->query('SELECT id_anime, id_utilisateur FROM liste_vus where liste_vus.id_utilisateur='.$j);
-	$vus=array();
+	$liste=$bdd->query('SELECT id_anime FROM liste_vus where liste_vus.id_utilisateur='.$j);	
+	$vus[$j]=array();
 	$rep = $bdd->query('select id_anime from anime');
 	$i=1;
-		while($i<=13211){
+		while($i<=13211){ //13211
 			if($rep->fetch()==$liste->fetch()) {
-				array_push($vus, 1);
+				array_push($vus[$j], $i);
 			}
 			else{
-				array_push($vus, 0);	
+				array_push($vus[$j], 0);	
 			}
 			$i=$i+1;
 		}
-		print_r($vus, $return = null);
-	$rep ->closeCursor();
+			//print_r($vus[$j], $return = null);
 	$j+=1;
 	}
 
+$f=1;
+$sim=array();//table de la taille du nombre d'utilisateur 
+while($f<$j) {
+	$cmp[$f]=array_diff_assoc($vus[$id_utilisateur],$vus[$f]);
+	array_push($sim, count($cmp[$f])); //ajout de la différence du nb d'anime vu entre l'utilisateur connecté et l'utilisateur f
+	$f+=1;
+	}
+$rep ->closeCursor();
 /**
 $nb=0;
 foreach($bdd->query('SELECT COUNT(*) FROM utilisateur') as $row) {
@@ -112,33 +74,46 @@ while($j<$nb) {
 	$j+=1;
 	}**/
 
+//Trouver le minimum de différence dans le tableau
+$min = $sim[0];
+for($i=0; $i < count($sim); $i++){	
+	if ($sim[$i] > $min and $sim[$i]!=0){
+   	$min = $sim[$i];
+	}
+}
+ 
+//Recherche de l'identifiant correspondant au minimum
+for($i=0; $i < count($sim); $i++){	
+	if ($sim[$i] == $min){
+  		$ind=$i+1;
+  }
+}
+
+																									
+$simi = $bdd->query('SELECT liste_vus.id_anime, image_url_anime from liste_vus, anime where liste_vus.id_anime=anime.id_anime and id_utilisateur='.$ind);
+
+//$moi = $bdd->query('SELECT liste_vus.id_anime, image_url_anime from liste_vus, anime where liste_vus.id_anime=anime.id_anime and id_utilisateur='.$id_utilisateur);
+?> </div><br><br> <div class="liste">
+	Recommandation de la liste d'un utilisateur similaire à vous : <br /> 
+	<?php
+	
+$non_vide2=FALSE;
+
+while($ligne2 =$simi->fetch()) {	
+
+echo "<a href=fiche_anime.php?id_anime=".$ligne2['id_anime']."><img src='".$ligne2['image_url_anime']."'></a>";
+#$id_anime_vus=$ligne2['id.anime']; 
+#$image_anime_vus=$ligne2['image_url_anime']; 
+$non_vide2=TRUE;}
+
 
 
 //$avu = $bdd->query('SELECT liste_vus.id_anime, image_url_anime from liste_vus, anime where liste_vus.id_anime=anime.id_anime and id_utilisateur='.$id_utilisateur);
-/*$avu = $bdd->query('SELECT id_anime FROM liste_vus where liste_vus.id_utilisateur='.$id_utilisateur);
 
-	
-
-$vus=array();
-$rep = $bdd->query('select id_anime from anime');
-$i=1;
-		while($i<13211){
-			if($rep->fetch()==$avu->fetch()) {
-				array_push($vus, 1);
-			}
-			else{
-				array_push($vus, 0);
-			}
-			$i=$i+1;
-		}
-print_r($vus, $return = null);
-$rep ->closeCursor(); 
-
-*/
 ?>
 
 <div class="bandeau"> 
-<object data="../bandeau.php" width="100%" height="100%">
+<object data="bandeau.php" width="100%" height="100%">
 </object>
 </div>
 
